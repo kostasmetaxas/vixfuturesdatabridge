5ce6aeb736026f9473394b2cda9d850e8c163a13
Kostas Sar <kostassar8@gmail.com>
2018-10-19 16:00:55 +0300

    Discarding unsused contracts before generating the time series

--- time_series.py
+++ time_series.py
@@ -3,6 +3,7 @@ import numpy as np
 import datetime
 import glob
 import csv
+import sys
 
 #Date format: Year-Month-Day
 date_format ='%Y-%m-%d'
@@ -33,12 +34,22 @@ def generate_time_series(first_target_contract, roll_days, forward):
         filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     csv_data_list = sorted(glob.glob("data/*.csv"))
+    #DELETE ALL ENTRIES UP TO first_target_contract
+    try:
+        target_index = csv_data_list.index(first_target_contract)
+    except ValueError:
+        sys.exit('Invalid target contract expiration date.')
+
+    while True:
+        split_string = csv_data_list[0].split("/",1)
+
 
     # Create an empty df (data frame) with the files' column names.
     df = pd.DataFrame()
     #Date format: Year-Month-Day
     date_format ='%Y-%m-%d'
     one_day = datetime.timedelta(days=1)
+    parsed_roll_days = datetime.timedelta(days = roll_days)
 
     # Go through the data and replace the df content each time a new file is accessed.
     for data in csv_data_list:
@@ -57,18 +68,19 @@ def generate_time_series(first_target_contract, roll_days, forward):
         else:
             parsed_last_date = parsed_last_date + one_day
 
+
+        if parsed_last_date == (parsed_contract_date - parsed_roll_days):
+            #ROLL, USE NEXT FILE, START FROM PARSED_LAST_DATE TRADE_DATE
+        else:
+            #COPY ROW, TO FIND ROW COMPARE PARSED_LAST_DATE WITH TRADE_DATE IN CSV
+
+
         #COMPARE EXP_DATE WITH LAST_DATE
 
         #COMPARE EXP_DATE WITT EXP_DATE - ROLL_DAYS
 
 
         # df.loc[df['Trade Date'] >= '' & df['Trade Date'] <= '']
-
-
-        # APPEND ALL ENTRIES UNTIL EXPIRATION MINUS ROLL_DAYS
-
-        # ROLL TO THE NEXT CONTRACT AND OPEN NEXT FILE
-
 
     return
 
